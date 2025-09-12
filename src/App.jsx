4acdf1d2a35e0aa4722c98941290f32cdda85100
Jithendra Nagaraj Jalihal <njjithendra95@gmail.com>
2025-09-12 21:25:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BookFinder from './components/Book.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <BookFinder/>
+    </div>
+  )
+}
+
+export default App
